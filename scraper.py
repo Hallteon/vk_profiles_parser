e24962cd341c4d14fdee0404552939e482973b31
Ivan <belogurov.ivan@list.ru>
2022-11-06 18:44:00 +0500

    updated the scraper

--- scraper.py
+++ scraper.py
@@ -1,39 +1,37 @@
 import random
 import vk_api
 
-
-def users_parser(ids, session):
-    users = session.method('users.get', {'user_ids': ids, 'fields': 'bdate,status,counters,relation,'})
-
-    return users
+from config import TOKEN
 
 
-def divide_list(lst, n):
-    for x in range(0, len(lst), n):
-        e_c = lst[x:n + x]
-
-        if len(e_c) < n:
-            e_c = e_c + [None for y in range(n - len(e_c))]
-        yield e_c
+def users_parser(id, session):
+    try:
+        user = session.method('users.get', {'user_ids': f'{id}', 'fields': 'bdate,status,relation,followers_count'})
+        # print(session.method('groups.get', {'user_ids': id}))
+        user[0]['friends'] = session.method('friends.get', {'user_id': f'{id}'})['count']
+        print(user)
+    except:
+        return False
+    else:
+        return user
 
 
 def get_data(number):
     ids = random.sample(range(1, 100000), number)
-    samples = divide_list(ids, 500)
     users = []
 
-    for sample in samples:
-        sample = [str(id) for id in sample]
-        str_sample = ','.join(sample)
-        user = users_parser(str_sample, api_session)
-        users.append(user)
+    for id in ids:
+        user = users_parser(id, api_session)
+
+        if user:
+            users.append(user)
 
     if users:
         return users
 
 
 if __name__ == '__main__':
-    api_session = vk_api.VkApi(token='vk1.a.3QmRiAaVUfLTCfAyMf2MiBJu1ZSR3SNHNpcwYMmorsLHvqA_EU7bvZ-ufB2X9FeHECO_zORAF6PsUw3dXE3fRqVQncbwDGACdaHLPmw8fQUitqIZaAQUhITgYPtea_ovPSjWBWE2j-F-aAkW8-tY2jEF3kDPMxqjMi5604oeAUKAwrfarj_ovi10OTDcMWeqLd8wj97XTRg6ghgSccF57w')
+    api_session = vk_api.VkApi(token=TOKEN)
     vk = api_session.get_api()
 
     print(get_data(1000))
